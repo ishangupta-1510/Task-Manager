84bf23e943e668deb5790d3d2d98aad7089d04ef
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-03-04 17:40:16 +0530

    db connection done

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(
-    "mongodb+srv://usa44052:ishan@cluster0.9egj9ls.mongodb.net/Task_manager?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  return mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
