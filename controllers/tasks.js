dbb5fe1675370ae7edb7a1890d4ae591effc5b56
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-03-04 18:00:15 +0530

    modals done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,15 @@
+const Task = require("../modals/task");
+
 const getalltask = (req, res) => {
   res.send("get task");
 };
-const posttask = (req, res) => {
-  res.json(req.body);
+const posttask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const patchtask = (req, res) => {
   res.json(req.params.id);
